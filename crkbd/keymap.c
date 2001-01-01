1980c48269639a7316accb0d910405440176701d
Paul Shannon <paulrobshannon@gmail.com>
2001-01-01 00:24:18 +0000

    updating crkbd keymap

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -36,13 +36,13 @@ enum macro_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB,    KC_Q,   KC_W,    KC_F,    KC_P,   KC_G,                          KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,\
+      KC_TAB,    KC_Q,   KC_W,    KC_F,    KC_P,   KC_G,                          KC_J,   KC_L,    KC_U,    KC_Y,    KC_SCLN,KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_BSPC,   KC_A,   KC_R,    KC_S,    KC_T,   KC_D,                          KC_H,   KC_N,    KC_E,    KC_I,    KC_O,   KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,                          KC_K,   KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_ENT,\
+      KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,   KC_B,                          KC_K,   KC_M,   KC_COMM,  KC_DOT, KC_SLSH, KC_ENT, \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,  LOWER, KC_LSFT,     KC_SPC,  RAISE,   NAV    \
+                                          KC_LGUI,  LOWER, KC_BSPC,     KC_SPC,  RAISE,   NAV    \
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_4,    KC_5,    KC_6,  XXXXXXX, XXXXXXX,\
+      KC_LCTL, _______, _______, _______, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_4,    KC_5,    KC_6,  XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,  KC_1,    KC_2,    KC_3,  XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER, KC_LSFT,    KC_SPC,  RAISE,  KC_RALT \
+                                          KC_LGUI, LOWER,   KC_LSFT,     KC_0 ,  RAISE,  KC_RALT \
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -79,19 +79,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
+                                         KC_LGUI, LOWER,   KC_LSFT,    KC_SPC,  RAISE, KC_RALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_NAV] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                      KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+     XXXXXXX, _______, _______, _______,  _______, _______,                     KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI,  XXXXXXX, XXXXXXX,                      KC_LEFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+     XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI,  _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LSFT, _______, _______, _______, _______,  XXXXXXX,                      KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+     KC_LSFT, _______, _______, _______, _______,  _______,                     KC_UP,   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         KC_LGUI,   LOWER, KC_LSFT,    KC_SPC,  RAISE,  KC_RALT \
+                                         KC_LGUI,   LOWER, KC_LSFT,    KC_SPC,  RAISE,   KC_RALT \
                                       //`--------------------------'  `--------------------------'
   ),
 };
