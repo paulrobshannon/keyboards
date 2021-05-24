5a22c0e02d64368056dec1b41dc49ded7afd2c78
Paul Shannon <paulrobshannon@gmail.com>
2021-05-24 22:33:25 +0100

    homerow mods

--- crkbd/config.h
+++ crkbd/config.h
@@ -34,6 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
