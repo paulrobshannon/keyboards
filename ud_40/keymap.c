7f6393f29d0c0768d1fcbc0b0c0278b53a6e738a
Paul Shannon <paulrobshannon@gmail.com>
2021-05-28 08:38:45 +0100

    adding homerow mods to ud_40

--- ud_40/keymap.c
+++ ud_40/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_BL] = LAYOUT_ortho_4x12(
       KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-      KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+      KC_BSPC, KC_A,    LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T), KC_D,    KC_H,  RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I),  KC_O,    KC_QUOT,
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
       KC_ESC,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SHFT_BS, KC_SPC,  RAISE,   NAV_LFT, KC_DOWN, KC_UP,   KC_RGHT
   ),
@@ -139,4 +139,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   // more layer combos can go here as state = update_tri_...
   return state;
 }
-
