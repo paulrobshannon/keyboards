3517b065ecd7ad86fd0a08a87e351b8dbc208dcd
Paul Shannon <paulrobshannon@gmail.com>
2021-08-30 10:10:24 +0100

    fixing layers

--- ferris_sweep/keymap.c
+++ ferris_sweep/keymap.c
@@ -18,10 +18,9 @@
 
 enum combo_events {
   COMBO_BSPC,
-  COMBO_NUMBAK,
   COMBO_TAB,
   COMBO_ESC,
-  COMBO_DEL,
+  COMBO_HASH
 };
 
 
@@ -30,9 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------+-------------+-------------+-------------+-------------.                    ,-------------+-------------+-------------+-------------+-------------|
          KC_Q,         KC_W,         KC_F,         KC_P,         KC_G,                             KC_J,          KC_L,         KC_U,         KC_Y,         KC_SCLN, \
   //|-------------+-------------+-------------+-------------+-------------+                    +-------------+-------------+-------------+-------------+-------------|
-         KC_A,     LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T),     KC_D,                             KC_H,      RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I),     KC_O,    \
+   LT(_NAV, KC_A), LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T),     KC_D,                             KC_H,      RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I),     KC_O,    \
   //|-------------+-------------+-------------+-------------+-------------+                    +-------------+-------------+-------------+-------------+-------------|
-         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,                             KC_K,          KC_M,         KC_COMM,      KC_DOT,       KC_SLSH, \
+     LSFT_T(KC_Z),     KC_X,         KC_C,         KC_V,         KC_B,                             KC_K,          KC_M,         KC_COMM,      KC_DOT,       KC_SLSH, \
   //|-------------+-------------+-------------+-------------+-------------+--------|  |--------+-------------+-------------+-------------+-------------+-------------|
                                                                  LOWER,    SHFT_BS,     KC_SPC,     RAISE    \
   //                                                        |-------------+--------|  |--------+-------------|
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-------------+-------------+-------------+-------------+-------------+                    +-------------+-------------+-------------+-------------+-------------|
         KC_GRV,       _______,      _______,      _______,      _______,                           _______,      _______,      KC_COMM,      KC_DOT,       KC_QUES,  \
   //|-------------+-------------+-------------+-------------+-------------+--------|  |--------+-------------+-------------+-------------+-------------+-------------|
-                                                                 LOWER,    SHFT_BS,     KC_SPC,     RAISE    \
+                                                                 LOWER,    SHFT_BS,     KC_SPC,    RAISE     \
   //                                                        |-------------+--------|  |--------+-------------|
   ),
 
@@ -79,21 +78,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef COMBO_ENABLE
 const uint16_t PROGMEM combo_bspc[] = {KC_Y, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
-const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
-const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
+const uint16_t PROGMEM combo_tab[]  = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_esc[]  = {KC_1, KC_2, COMBO_END};
+const uint16_t PROGMEM combo_hash[] = {KC_EXLM, KC_AT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
-  [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
-  [COMBO_ESC] = COMBO(combo_esc,KC_ESC)
+  [COMBO_BSPC] = COMBO(combo_bspc, KC_BSPC),
+  [COMBO_TAB]  = COMBO(combo_tab,  KC_TAB),
+  [COMBO_ESC]  = COMBO(combo_esc,  KC_ESC),
+  [COMBO_HASH] = COMBO(combo_hash, LALT(KC_3))
 
 };
 #endif
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  state = update_tri_layer_state(state, _LOWER, _RAISE, _NAV);
-  // more layer combos can go here as state = update_tri_...
-  return state;
-}
